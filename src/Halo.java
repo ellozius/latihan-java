68a482bd61f09d6ba0f5e1d6bdbec78d780d5458
Edy Zulyza <edy.zulyza@gmail.com>
2015-08-31 15:19:03 +0700

    add line

--- src/Halo.java
+++ src/Halo.java
@@ -2,7 +2,9 @@ package id.go.riau.bkd.test1;
 
 public class Halo {
 	public static void main(String[] xx){
+		System.out.println("......");
 		System.out.println("Aloha...");
+		System.out.println("......");
 	}
 }
 
